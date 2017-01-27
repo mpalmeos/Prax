c11d75bb0694a7ea55bb6bfbc68489b43dc713b5
Maie Palmeos <mpalmeos@itcollege.ee>
2017-01-27 16:29:35 +0200

    Liivakell

--- KysimusedVastused.java
+++ KysimusedVastused.java
@@ -2,5 +2,24 @@
  * Created by mpalmeos on 27.01.2017.
  */
 public class KysimusedVastused {
-    
+    //Kikilips
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(" " + i + "-" + j + " ");
+            }
+            System.out.println();
+        }
+// samm-sammu haaval -> ei ole vaja tervet valemit ühekorraga välja tuletada!
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if ((i >= j && i + j < 9) || (i <= j && i + j >= 8)) {
+                    System.out.print(" 0 ");
+                } else {
+                    System.out.print(" - ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
